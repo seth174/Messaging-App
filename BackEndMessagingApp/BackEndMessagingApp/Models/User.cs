6ecfdefafabd8e95944acc20138b7b4dee0400e8
Seth Fagen <sfagen@westmonroe.com>
2022-09-13 18:15:49 -0700

    Updated db to allow null values fro columns

--- BackEndMessagingApp/BackEndMessagingApp/Models/User.cs
+++ BackEndMessagingApp/BackEndMessagingApp/Models/User.cs
@@ -16,10 +16,10 @@ namespace BackEndMessagingApp.Models
         public string Password { get; set; }
 
         public ICollection<UserPerConversation>? userPerConversations { get; set; }
-        public ICollection<DeletedMessage> deletedMessages { get; set; }
-        public ICollection<Message> messages { get; set; }
-        public ICollection<DeletedConversation> deletedConversations { get; set; }
-        public ICollection<MessageReactionPerUser> messagesReactionPerUser { get; set; }
+        public ICollection<DeletedMessage>? deletedMessages { get; set; }
+        public ICollection<Message>? messages { get; set; }
+        public ICollection<DeletedConversation>? deletedConversations { get; set; }
+        public ICollection<MessageReactionPerUser>? messagesReactionPerUser { get; set; }
 
     }
 }
