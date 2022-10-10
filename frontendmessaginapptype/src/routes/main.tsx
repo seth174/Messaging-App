f7e9d35d1829c6d462f9f3e8990447addc89e165
Seth Fagen <sfagen@westmonroe.com>
2022-10-10 13:51:46 -0700

    Added conversation header

--- frontendmessaginapptype/src/routes/main.tsx
+++ frontendmessaginapptype/src/routes/main.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, CssBaseline, Toolbar, Typography } from "@mui/material";
 import React, { FC, useEffect, useState } from "react";
 import Conversation from "../components/conversation";
 import MainPageNavBar from "../components/main-page-nav-bar";
@@ -21,7 +21,6 @@ const Main: FC = () => {
     const id: number = parseInt(window.sessionStorage.getItem("user_id") || "-100");
     async function getLoggedInUser() {
 
-
       const user: IUser = await getUser(id);
       setUser(user);
       setUsersPerConversation(user.userPerConversations);
@@ -30,6 +29,25 @@ const Main: FC = () => {
     getLoggedInUser();
   }, []);
 
+  const calculateConversationName = (users: IUserPerConversations[] | undefined, length: number): string => {
+    let name: string = "";
+    const userId: string | null = window.sessionStorage.getItem("user_id");
+    let userNumber: number = parseInt(userId ? userId : '-1');
+    let count = 0;
+    users?.forEach((user: IUserPerConversations) => {
+      if (user.user?.id != userNumber) {
+        count += user.user?.name ? user.user?.name.length : 0;
+        name += user.user?.name + ', '
+        count += 2;
+      }
+    });
+    if (count > length) {
+      return name.substring(0, length) + '...'
+    }
+    name = name.substring(0, name.length - 2)
+    return name;
+  }
+
   return (
     <div>
 
@@ -37,8 +55,8 @@ const Main: FC = () => {
         <MainPageNavBar />
         <CssBaseline />
 
-        <SideMenu userPerConversations={usersPerConversation} setConversation={setConversation} />
-        <Conversation conversation={conversation} />
+        <SideMenu userPerConversations={usersPerConversation} setConversation={setConversation} calculateConversationName={calculateConversationName} />
+        <Conversation conversation={conversation} calculateConversationName={calculateConversationName} />
       </Box>
     </div>
 
---
+++ frontendmessaginapptype/src/themes/theme.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+
+  typography: {
+
+    button: { // Here is where you can customise the button
+      fontSize: 16,
+      fontWeight: 700,
+      textTransform: 'none'
+    },
+  },
+});
