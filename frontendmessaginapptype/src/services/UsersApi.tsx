45e8f10537909ab6134caa748d3b138ccac737b7
Seth Fagen <sfagen@westmonroe.com>
2022-10-11 13:58:11 -0700

    Began adding search bar to conversations page

--- frontendmessaginapptype/src/services/UsersApi.tsx
+++ frontendmessaginapptype/src/services/UsersApi.tsx
@@ -16,7 +16,7 @@ function getHeader() {
 
 export const getUsers = async (): Promise<IUser[]> => {
   const response = await axios
-    .get(`${BASE_URL}users/`)
+    .get(`${BASE_URL}users/`, getHeader())
     .then((result: { data: IUser[]; }) => {
       const response: IUser[] = result.data;
       return response;
