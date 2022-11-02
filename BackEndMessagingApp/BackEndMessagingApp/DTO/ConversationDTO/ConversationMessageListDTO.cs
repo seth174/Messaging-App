f176df782609005c9fce41b87ae52626d7a5228c
Seth Fagen <sfagen@westmonroe.com>
2022-11-01 21:34:36 -0700

    Added backend support for API to get Conversation messages

---
+++ BackEndMessagingApp/BackEndMessagingApp/DTO/ConversationDTO/ConversationMessageListDTO.cs
@@ -0,0 +1,15 @@
+﻿using BackEndMessagingApp.DTO.MessageDTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEndMessagingApp.DTO.ConversationDTO
+{
+    public class ConversationMessageListDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(150)]
+        public string? Title { get; set; }
+
+        public ICollection<MessageListDTO>? Messages { get; set; }
+    }
+}
---
+++ BackEndMessagingApp/BackEndMessagingApp/DTO/MessageDTO/MessageListDTO.cs
@@ -0,0 +1,14 @@
+﻿using BackEndMessagingApp.DTO.UserDTO;
+
+namespace BackEndMessagingApp.DTO.MessageDTO
+{
+    public class MessageListDTO
+    {
+        public int Id { get; set; }
+        public string MessageText { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? ImageUrl { get; set; }
+        public UserListDTO User { get; set; }
+        
+    }
+}
